96ba652be5fd4471b61eb7994b581efaeda8802a
tom <tom@twovi.com>
2017-11-19 18:51:12 -0600

    updating sql statement for initial upload

--- sql/initial_setup.sql
+++ sql/initial_setup.sql
@@ -2,7 +2,8 @@ drop table words if exists;
 CREATE TABLE
  IF NOT EXISTS words (
     id integer PRIMARY KEY,
-    name text NOT NULL
+    name text NOT NULL,
+    created DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 drop table scores if exists;
